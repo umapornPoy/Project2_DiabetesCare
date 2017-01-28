e0a36474f1bdd6e1c117702593e76096b1c3c9a6
RATTANAPORN SOMNUK <5630213004@email.psu.ac.th>
2017-01-28 22:27:59 +0700

    Revert "add layout register"
    This reverts commit b0676c5f020dec85c286403d74a79b5781dfb72a.

--- DiabetesCare/DiabetesJS/app.js
+++ DiabetesCare/DiabetesJS/app.js
@@ -1,6 +1,7 @@
 ﻿var app = angular.module('Diabetes', ['ngRoute','ui.bootstrap']);
 app.config(function ($routeProvider) {
     $routeProvider
+
     .when("/", {
         templateUrl: "Home/Index"
     })
